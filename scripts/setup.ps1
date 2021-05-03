6bacd6d7b1b203944da78abb5c3c9d43d3dea9d9
Anoop Simon <63703161+automationio@users.noreply.github.com>
2021-05-04 08:44:05 +1000

    Update setup.ps1

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,17 +1,17 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install dotnetcore-sdk --version=3.1.200
+choco install dotnetcore-sdk --version=3.1.200 -y
 
-choco install sql-server-management-studio
+choco install sql-server-management-studio -y
 
-choco install visualstudio2019professional
+choco install visualstudio2019professional -y
 
-choco install postman
+choco install postman -y
 
-choco install git
+choco install git -y
 
-choco install googlechrome
+choco install googlechrome -y
 
-choco install notepadplusplus
+choco install notepadplusplus -y
 
-choco install azure-cli
+choco install azure-cli -y
